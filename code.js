9e1c4a0ea2e9727a17fbe53e81519bcf869cb03c
HemantDimri27 <dimrihemant27@gmail.com>
2024-07-02 18:56:59 +0530

    add comment

--- code.js
+++ code.js
@@ -1,9 +1,11 @@
 const clock = document.getElementById('clock');
 // const clock = document.querySelector('#clock')
 
-setInterval(function () {
+
+// setInterval(function(){}, time);
+setInterval(function () {                                           // repeat function in given interval of time
   let date = new Date();
   // console.log(date.toLocaleTimeString());
-  clock.innerHTML = date.toLocaleTimeString();
+  clock.innerHTML = date.toLocaleTimeString();                        
 }, 1000);
 
